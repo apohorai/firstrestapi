82a50fdb4a40cb0b798cfbc44da732a6bc21b55f
Attila Pohorai <attila.pohorai@gmail.com>
2022-06-13 12:38:35 +0800

    get and post working

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-const app = require('express')()
+const express=require('express');
+const app = express();
 const PORT = 8080;
-app.listen(
-	PORT,
-	() => console.log(`it's alive`)
+app.use(express.json())
+app.listen(PORT, () => console.log(`it's alive`));
+app.get("/positions", (req, res) => 
+	{
+	res.status(200).send({
+		x: '1',
+		y: '2'
+     }
+			    )
+    }
+       );
+app.post('/positions/:id',(req,res) =>{
+
+	const{id} = req.params;
+	const{x} = req.body;
+	const{y} = req.body;
+	if (!x){
+		res.status(418).send({message: "x is missing"})
+	}
+	
+	if (!y){
+		res.status(418).send({message: "y is missing"})
+	}
+	res.send(
+		{
+			position: `${x},${y},${id}`
+		}
+	)
+}
 )
+
+
+
+
+
+
